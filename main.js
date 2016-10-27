d6c9531c5dde4faaacacc77a5e754669fdba593e
yurifrl <yurifrl@hotmail.com>
2016-10-27 11:23:43 -0200

    functional af

--- main.js
+++ main.js
@@ -16,38 +16,55 @@ const map = cb => arr => arr.map(x => cb(x))
 
 
 // Event dispacher
-const addCartEvent = (state, setState) => {
-  return variantId => {
-    yebo.addCartItems(state.orderToken, state.orderNumber, false, true, state.userToken, variantId, 1)
-      .then(setState)
-      .then(getAuthToken)
-      .then(listPayments)
+const addCartEvent = (sdk) => {
+  return state => {
+    return setState => {
+      return getAuthToken => {
+        return listPayments => {
+          return variantId => {
+            sdk.addCartItems(state.orderToken, state.orderNumber, false, true, state.userToken, variantId, 1)
+              .then(setState(state))
+              .then(getAuthToken(sdk)(setState))
+              .then(listPayments(sdk))
+          }
+        }
+      }
+    }
   }
 }
 
-const setState = (newState) => {
-  if (newState.order != undefined) {
-    state.orderToken = newState.order.token
-    state.orderNumber = newState.order.number
-  } else {
-    state.userToken = newState.user.token
-  }
-  return state
-}
-
-const getAuthToken = (state) => {
-  return yebo.executeRequest(yebo.buildAuthentication()).then(res => {
-    state.authToken = res.token
-    yebo.set('auth', state.authToken)
+const setState = (state) => {
+  return newState => {
+    if (newState.order != undefined) {
+      state.orderToken = newState.order.token
+      state.orderNumber = newState.order.number
+    } else {
+      state.userToken = newState.user.token
+    }
     return state
-  })
+  }
 }
 
-const listPayments = (state) => {
-  debugger
-  yebo.getOrderPayments(state.orderNumber, state.userToken, false).then(res => {
+const getAuthToken = (sdk) => {
+  return setState => {
+    return state => {
+      return sdk.executeRequest(yebo.buildAuthentication()).then(res => {
+        state.authToken = res.token
+        sdk.set('auth', state.authToken)
+        return state
+      })
+    }
+  }
+
+}
+
+const listPayments = (sdk) => {
+  return state => {
     debugger
-  })
+    return sdk.getOrderPayments(state.orderNumber, state.userToken, false).then(res => {
+      debugger
+    })
+  }
 }
 
 // document
@@ -78,9 +95,11 @@ const newProductEl = (doc, clickEvent) => {
   }
 }
 
+clickBehaviour = addCartEvent(yebo)(state)(setState)(getAuthToken)(listPayments)
+
 yebo.getProducts({})
   .then(prop("products"))
-  .then(xs => xs.map(newProductEl(document, clickEvent(addCartEvent(state, setState)))))
+  .then(xs => xs.map(newProductEl(document, clickEvent(clickBehaviour))))
   .then(xs => xs.map(x => mainDiv.appendChild(x)))
 /* --------------- Inpure --------------- */
 
